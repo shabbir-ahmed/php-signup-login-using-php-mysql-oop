354256a90b71d3a85f1a3c44c4ad8e6bd37e1a27
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-11 05:56:55 +0000

    New

--- include/config.php
+++ include/config.php
@@ -1,6 +1,7 @@
 <?php
-define('DB_SERVER', 'server_name');
-define('DB_USERNAME', 'user_name');
-define('DB_PASSWORD', 'user_password');
-define('DB_DATABASE', 'database_name');
+define('DB_SERVER', '127.0.0.1');
+define('DB_USERNAME', 'shabbir_ahmed');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'c9');
+define('PORT', 3306);
 ?>
