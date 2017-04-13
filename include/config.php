3cff71c8c36b583d9d1d5f4d8cb28eab552ebbb6
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-13 04:38:29 +0000

    New Update

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'DB_USERNAME');
-define('DB_PASSWORD', 'DB_PASSWORD');
-define('DB_DATABASE', 'DB_DATABASE');
+define('DB_USERNAME', 'shabbir_ahmed');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'c9');
 define('PORT', 3306);
 ?>
--- signup.php
+++ signup.php
@@ -3,25 +3,84 @@ include_once 'include/UserClass.php';
 $user = new User();
 
 // checking logged in or not
-
-if (isset($_REQUEST['submit'])) 
-{
-	extract($_REQUEST);
-	$register = $user->reg_user($fullname, $uname, $upass, $uemail);
-	if ($register) 
-	{
-		// Registration success
-		echo "Registration successful <a href='login.php'>Click here</a> to login";
-	}
-	else
-	{
-		echo "Registration faild";
-	}
-}
 ?>
 
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Secure Registration</title>
 
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  	<body>
+  		<div class="container">
+  			<?php
+  				if (isset($_REQUEST['submit'])) 
+				{
+					extract($_REQUEST);
+					$register = $user->reg_user($fullname, $uname, $upass, $uemail);
+					if ($register) 
+					{
+						// Registration success
+						?>
+							<p class="bg-success" style="border-radius: 10px;margin-top: 20px;padding: 24px;">Registration successful <a href='login.php'>Click here</a> to login</p>
+						<?php
+					}
+					else
+					{
+						?>
+							<p class="bg-danger" style="border-radius: 10px;margin-top: 20px;padding: 24px;">Registration faild</p>
+						<?php
+					}
+				}
+  			?>
+			<h1>Registration Form</h1>
+			<form class="form-horizontal" action="" method="post" name="reg">
+				<div class="form-group">
+				    <label class="col-sm-2 control-label">Full Name:</label>
+				    <div class="col-sm-3">
+				      <input type="text" class="form-control" name="fullname" placeholder="Full Name" required>
+				    </div>
+				 </div>
+				 
+				 <div class="form-group">
+				    <label class="col-sm-2 control-label">User Name:</label>
+				    <div class="col-sm-3">
+				      <input type="text" class="form-control" name="uname" placeholder="Username" required>
+				    </div>
+				 </div>
+				 
+				 <div class="form-group">
+				    <label class="col-sm-2 control-label">Email:</label>
+				    <div class="col-sm-3">
+				      <input type="text" class="form-control" name="uemail" placeholder="Email" required>
+				    </div>
+				 </div>
+				 
+				 <div class="form-group">
+				    <label class="col-sm-2 control-label">Password:</label>
+				    <div class="col-sm-3">
+				      <input type="password" class="form-control" name="upass" placeholder="Password" required>
+				    </div>
+				 </div>
+			  		
+		  		<div class="form-group">
+				    <div class="col-sm-3 col-sm-offset-2">
+				      <button onclick="return(submitreg());" type="submit" name="submit" class="btn btn-primary">Register</button>
+				      <br>
+				      <a href="login.php">Already registered! Click Here!</a>
+				    </div>
+				</div>
+			</form>
+		</div>
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  	</body>
+</html>
 <script type="text/javascript" language="javascript">
 	function submitreg() {
 		var form = document.reg;
@@ -43,37 +102,4 @@ if (isset($_REQUEST['submit']))
 	 	}
 	}
 </script>
-<div id="container">
-	<h1>Registration Here</h1>
-	<form action="" method="post" name="reg">
-		<table>
-			<tbody>
-				<tr>
-					<th>Full Name:</th>
-					<td><input type="text" name="fullname" required="" /></td>
-				</tr>
-				<tr>
-					<th>User Name:</th>
-					<td><input type="text" name="uname" required="" /></td>
-				</tr>
-				<tr>
-					<th>Email:</th>
-					<td><input type="text" name="uemail" required="" /></td>
-				</tr>
-				<tr>
-					<th>Password:</th>
-					<td><input type="password" name="upass" required="" /></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input onclick="return(submitreg());" type="submit" name="submit" value="Register" /></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><a href="login.php">Already registered! Click Here!</a></td>
-				</tr>
-			</tbody>
-		</table>
-	</form>
-</div>
 
